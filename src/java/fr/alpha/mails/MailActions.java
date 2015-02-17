e26367ed4982525ff5a4c1f62d839cce5a60845a
INTI <INTI@192.168.20.129>
2015-02-17 10:09:32 +0100

    Envoi mail + ajout en base de produit et info rep

--- src/java/fr/alpha/mails/MailActions.java
+++ src/java/fr/alpha/mails/MailActions.java
@@ -11,15 +11,14 @@ import fr.alpha.model.Utilisateur;
 import java.util.Map;
 import org.apache.struts2.interceptor.SessionAware;
 
-
 /**
  *
  * @author INTI
  */
-public class MailActions extends ActionSupport implements ModelDriven<Mail> , SessionAware{
+public class MailActions extends ActionSupport implements ModelDriven<Mail>, SessionAware {
+
     private Map<String, Object> session;
     private Mail mailForm;
-
 
     @Override
     public Mail getModel() {
@@ -27,35 +26,33 @@ public class MailActions extends ActionSupport implements ModelDriven<Mail> , Se
         return mailForm;
     }
 
-
     @Override
     public String execute() throws Exception {
 
         EnvoiMail em = new EnvoiMail();
         String mailTemp = mailForm.getMailTo();
-        String[]recipients;
-        
+        String[] recipients;
 
         Utilisateur user = (Utilisateur) session.get("USER");
 
         recipients = new String[]{mailTemp};
 
         String subject = "Informations Reparations";
-        String message = "<html>\n" +
-"    <head>\n" +
-"        <title>TODO supply a title</title>\n" +
-"        <meta charset=\"UTF-8\">\n" +
-"        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
-"    </head>\n" +
-"    <body>\n" +
-"        Bonjour Mr/Mme " +user.getNom()+"\n" +
-"        <div><a href=\"http://localhost:8080/ProjectAlpha/\">\nVoici un lien vers notre site</a></div>\n" +
-"    </body>\n" +
-"</html>\n" +
-"";
+        String message = "<html>\n"
+                + "    <head>\n"
+                + "        <title>TODO supply a title</title>\n"
+                + "        <meta charset=\"UTF-8\">\n"
+                + "        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n"
+                + "    </head>\n"
+                + "    <body>\n"
+                + "        Bonjour Mr/Mme " + user.getNom() + "\n"
+                + "        <div><a href=\"http://localhost:8080/ProjectAlpha/\">\nVoici un lien vers notre site</a></div>\n"
+                + "    </body>\n"
+                + "</html>\n"
+                + "";
         System.out.println("----------------");
         String from = "no-reply.alpha@orange.fr";
-        
+
         try {
             System.out.println("mail à envoyé !!!");
             em.sendMail(user.getMail(), subject, message, from);
@@ -71,7 +68,7 @@ public class MailActions extends ActionSupport implements ModelDriven<Mail> , Se
 
     @Override
     public void setSession(Map<String, Object> map) {
-        this.session = map ;
+        this.session = map;
     }
 
 }
